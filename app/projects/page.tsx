8819e3dfb39abfb31fcab84fc5b501b2431159ee
russelrex <russelrex_23@yahoo.com>
2025-05-06 18:08:42 +0800

    Add more projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -35,8 +35,58 @@ export default async function ProjectsPage() {
         <div className="w-full h-px bg-zinc-800" />
 
         <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+        <Card>
+            <Link href='https://www.a-traq.com/' target="_blank">
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
+                    April 01, 2025
+                  </div>
+                </div>
+
+                <h2
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                >
+                  ATRAQ
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  TechStack: Wix Studio, Velo, Nodejs.
+                </p>
+                <div className="flex justify-center mt-4">
+                  <img src="atraq.png" alt="Description of image" className="w-32 h-32" />
+                </div>
+              </article>
+            </Link>
+          </Card>
+
           <Card>
-            <Link href='https://fassadigital.wixstudio.com/this-is-the-day' target="_blank">
+            <Link href='https://fassadigital.wixstudio.com/road-hero' target="_blank">
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
+                    April 01, 2025
+                  </div>
+                </div>
+
+                <h2
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                >
+                  Road Hero
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  TechStack: Wix Studio, Wix Stores, Velo, Nodejs.
+                </p>
+                <div className="flex justify-center mt-4">
+                  <img src="Road.png" alt="Description of image" className="w-32 h-32" />
+                </div>
+              </article>
+            </Link>
+          </Card>
+
+          <Card>
+            <Link href='https://www.thisistheday.com.au/' target="_blank">
               <article className="relative w-full h-full p-4 md:p-8">
                 <div className="flex items-center justify-between gap-2">
                   <div className="text-xs text-zinc-100">
@@ -84,11 +134,11 @@ export default async function ProjectsPage() {
             </Link>
           </Card>
           <Card>
-            <Link href='https://ilovegolfwhen.com/' target="_blank">
+            <Link href='https://www.a-traq.com/' target="_blank">
               <article className="relative w-full h-full p-4 md:p-8">
                 <div className="flex items-center justify-between gap-2">
                   <div className="text-xs text-zinc-100">
-                    Jan 01, 2025
+                  January 01, 2025
                   </div>
                 </div>
 
@@ -96,16 +146,53 @@ export default async function ProjectsPage() {
                   id="featured-post"
                   className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
                 >
-                  ILoveGolfWhen
+                  ALONGSIDE
                 </h2>
                 <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                  TechStack: WordPress, Elementor.
+                  TechStack: NextJs, Nodejs, Express and MongoDB.
                 </p>
                 <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                  With working E-Commerce, payment, and shipping integration.
+                  UI still not in development, this focuses more on functionalities
+                  This is similar to dating app but in sports where a user can find other users that 
+                  matched with their sports focuses more on functionalities:
+                  <ul className="list-disc list-inside space-y-2">
+                    <li>Registration</li>
+                    <li>Login</li>
+                    <li>
+                      Profile:
+                      <ul className="list-disc list-inside ml-4 space-y-1">
+                        <li>Update and delete functionalities</li>
+                        <li>Profile pictures</li>
+                        <li>Featured pictures</li>
+                        <li>News feed</li>
+                        <li>Overview showing users who like you</li>
+                      </ul>
+                    </li>
+                    <li>
+                      Invites:
+                      <ul className="list-disc list-inside ml-4 space-y-1">
+                        <li>Invite users/players based on selected sports</li>
+                        <li>Accept or decline invitations</li>
+                      </ul>
+                    </li>
+                    <li>
+                      Schedules:
+                      <ul className="list-disc list-inside ml-4 space-y-1">
+                        <li>Schedule sports activities with selected users</li>
+                        <li>RSVP functionality (Going / Not Going / Maybe)</li>
+                      </ul>
+                    </li>
+                    <li>Clubs</li>
+                    <li>
+                      Events:
+                      <ul className="list-disc list-inside ml-4 space-y-1">
+                        <li>General page listing all created events</li>
+                      </ul>
+                    </li>
+                  </ul>
                 </p>
                 <div className="flex justify-center mt-4">
-                  <img src="ilovegolf.png" alt="Description of image" className="w-64 h-24" />
+                  <img src="along.png" alt="Description of image" className="w-32 h-32" />
                 </div>
               </article>
             </Link>
@@ -137,6 +224,63 @@ export default async function ProjectsPage() {
               </article>
             </Link>
           </Card>
+
+          <Card>
+            <Link href='https://home.navqms.com/' target="_blank">
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
+                    Aug 09, 2024
+                  </div>
+                </div>
+
+                <h2
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                >
+                  NavQMS
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  TechStack: NextJs, MaterialUI, TailwindCSS, Redux, Nodejs, Express and MongoDB.
+                </p>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  is a specialized Quality Management System (QMS) designed to streamline and enhance compliance processes within Navy shipbuilding operations
+                </p>
+                <div className="flex justify-center mt-4">
+                  <img src="nav.png" alt="Description of image" className="w-84 h-64 object-fit" />
+                </div>
+              </article>
+            </Link>
+          </Card>
+
+          <Card>
+            <Link href='https://ilovegolfwhen.com/' target="_blank">
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
+                    Jan 01, 2025
+                  </div>
+                </div>
+
+                <h2
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                >
+                  ILoveGolfWhen
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  TechStack: WordPress, Elementor.
+                </p>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  With working E-Commerce, payment, and shipping integration.
+                </p>
+                <div className="flex justify-center mt-4">
+                  <img src="ilovegolf.png" alt="Description of image" className="w-64 h-24" />
+                </div>
+              </article>
+            </Link>
+          </Card>
+          
           <Card>
             <Link href='https://store-app-lake.vercel.app/' target="_blank">
               <article className="relative w-full h-full p-4 md:p-8">
